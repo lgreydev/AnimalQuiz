778a42d0a791e0115cb4cd55636fcde5f195166f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-14 12:52:29 +0300

    Initial Commit

---
+++ AnimalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalQuiz
+//
+//  Created by Sergey Lukaschuk on 14.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
