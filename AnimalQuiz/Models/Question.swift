c01b8d2f595f5c9edb15b8be7702c05ca0197744
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-16 15:29:11 +0300

    Update model

--- AnimalQuiz/Models/Question.swift
+++ AnimalQuiz/Models/Question.swift
@@ -12,4 +12,32 @@ struct Question {
     let text: String
     let type: ResponseType
     let answers: [Answer]
+    
+    static let question: [Question] = [
+        Question(text: "What food do you like?", type: .single, answers: Self.single),
+        Question(text: "", type: .multiply, answers: Self.multiply),
+        Question(text: "", type: .rage, answers: Self.rage)
+    ]
+    
+    
+    static private let single = [
+    Answer(text: "Fish 🐟", type: .cat),
+    Answer(text: "Meat 🍖", type: .dog),
+    Answer(text: "Carrot 🥕", type: .rabbit),
+    Answer(text: "Corn 🌽", type: .turtle)
+    ]
+    
+    static private let multiply = [
+    Answer(text: "", type: .cat),
+    Answer(text: "", type: .dog),
+    Answer(text: "", type: .rabbit),
+    Answer(text: "", type: .turtle)
+    ]
+    
+    static private let rage = [
+    Answer(text: "", type: .cat),
+    Answer(text: "", type: .dog),
+    Answer(text: "", type: .rabbit),
+    Answer(text: "", type: .turtle)
+    ]
 }
