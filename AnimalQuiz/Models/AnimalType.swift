442383d244d9d6a482efabc51c14fc70b9f881b1
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-06-16 11:33:44 +0300

    Setup animal type

--- AnimalQuiz/Models/AnimalType.swift
+++ AnimalQuiz/Models/AnimalType.swift
@@ -13,5 +13,21 @@ enum AnimalType: Character {
     case dog = "🐕"
     case rabbit = "🐇"
     case turtle = "🐢"
+    
+    var definition: String {
+        switch self {
+        case .cat: return TextForAnimals.cat
+        case .dog: return TextForAnimals.dog
+        case .rabbit: return TextForAnimals.rabbit
+        case .turtle: return TextForAnimals.turtle
+        }
+    }
+    
+    private struct TextForAnimals {
+        static let cat = "You are independent and graceful, you have everything under control."
+        static let dog = "You are a good and reliable friend!"
+        static let rabbit = "You make decisions quickly, you don't have time for little things."
+        static let turtle = "You are in no hurry and do everything on time."
+    }
 }
 
